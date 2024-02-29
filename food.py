06ced2fc13ada595e39dc978986990c1a603d9b1
pebblewebble <ericlye123344@gmail.com>
2024-03-01 01:55:22 +0800

    Added food into main py file

--- food.py
+++ food.py
@@ -3,9 +3,11 @@ import pygame
 
 
 class Food:
+    foodCount = 0
     def __init__(self, screenWidth, screenHeight):
         self.xaxis = random.randint(0, screenWidth)
         self.yaxis = random.randint(0, screenHeight)
+        Food.foodCount+=1
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 255, 255), (self.xaxis, self.yaxis, 5, 5))
--- geneticalgo.py
+++ geneticalgo.py
@@ -1,10 +1,24 @@
 import pygame
+import random
+import food
+
 pygame.init()
-SCREEN_WIDTH=800
-SCREEN_HEIGHT=750
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+randomRect = pygame.Rect((random.randint(0, 799), random.randint(0, 749), 5, 5))
 run = True
 while run:
+    # pygame.draw.rect(
+    #     screen,
+    #     (255, 0, 0),
+    #     pygame.Rect((random.randint(0, 799), random.randint(0, 749), 5, 5)),
+    # )
+    # pygame.draw.rect(screen,(255,0,0),randomRect)
+    if food.Food.foodCount < 5:
+        food.Food(SCREEN_WIDTH, SCREEN_HEIGHT).draw(screen)
     for event in pygame.event.get():
-        if event.type== pygame.quit():
+        if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
+pygame.quit()
