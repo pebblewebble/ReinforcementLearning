b5e29d9b9988b340fce47e0f5a11e689892baf47
pebblewebble <ericlye123344@gmail.com>
2024-03-01 02:14:20 +0800

    Added creature class

--- creature.py
+++ creature.py
@@ -1,14 +1,15 @@
-import random
 import pygame
+import random
 
 
-class Food:
-    foodCount = 0
+class Creature:
+    creatureCount = 0
 
     def __init__(self, screenWidth, screenHeight):
         self.xaxis = random.randint(0, screenWidth - 1)
         self.yaxis = random.randint(0, screenHeight - 1)
-        Food.foodCount += 1
+        Creature.creatureCount += 1
+        self.energy = 50
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), (self.xaxis, self.yaxis, 5, 5))
+        pygame.draw.rect(screen, (255, 0, 0), (self.xaxis, self.yaxis, 10, 10))
--- geneticalgo.py
+++ geneticalgo.py
@@ -1,12 +1,12 @@
 import pygame
 import random
 import food
+import creature
 
 pygame.init()
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-randomRect = pygame.Rect((random.randint(0, 799), random.randint(0, 749), 5, 5))
 run = True
 while run:
     # pygame.draw.rect(
@@ -15,6 +15,8 @@ while run:
     #     pygame.Rect((random.randint(0, 799), random.randint(0, 749), 5, 5)),
     # )
     # pygame.draw.rect(screen,(255,0,0),randomRect)
+    if creature.Creature.creatureCount < 5:
+        creature.Creature(SCREEN_WIDTH, SCREEN_HEIGHT).draw(screen)
     if food.Food.foodCount < 5:
         food.Food(SCREEN_WIDTH, SCREEN_HEIGHT).draw(screen)
     for event in pygame.event.get():
